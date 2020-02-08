db9db6c7b86269b625255a4b04b65b3900073fb2
rezi99 <rezi99@gmail.com>
2020-02-08 20:20:29 +0100

    initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,12 @@
+<style>
+  h1 {
+    color: red;
+  }
+</style>
+
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+<h1>About this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,58 @@
+<style>
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Sapper project template</title>
+  <meta name="description" content="Welcome to sapper app" />
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<figure>
+  <img
+    sizes="(max-width: 400px) 100vw, 400px"
+    srcset=" great-success_dbsixb_c_scale,w_190.webp 190w,
+    great-success_dbsixb_c_scale,w_400.webp 400w"
+    src="great-success_dbsixb_c_scale,w_400.webp"
+    alt="" />
+  <figcaption>HIGH FIVE!</figcaption>
+</figure>
+
+<p>
+  <strong>
+    Try editing this file (src/routes/index.svelte) to test live reloading.
+  </strong>
+</p>
