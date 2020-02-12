e604ab38c45f1263ecf98d26263cf32c1f07b71e
Tomas Rezac <rezac@cngroup.dk>
2020-02-12 15:41:01 +0100

    weather

---
+++ src/routes/weather.svelte
@@ -0,0 +1,12 @@
+<style>
+  .weather-page {
+    height: 100wh;
+    background-color: cadetblue;
+  }
+</style>
+
+<svelte:head>
+  <title>Weather</title>
+</svelte:head>
+
+<section class="weather-page">test</section>
